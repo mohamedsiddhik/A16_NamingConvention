80355467d492e43f391645dbe54f25b423e4041e
mohamedsiddhik <117736610+mohamedsiddhik@users.noreply.github.com>
2022-12-01 15:55:54 +0530

    Add files via upload

---
+++ NamingConvention.java
@@ -0,0 +1,60 @@
+
+
+// Naming Convention
+
+/*
+     1.class
+
+     public class Employee
+     {
+       code
+     }
+
+     2.Interface
+
+     interface Printable
+     {
+        code
+     }
+
+     3.Method
+
+     class Employee
+     {
+       void draw()
+       {
+           code
+       }
+     }
+
+     4.Variable
+     class Employee
+     {
+        int id;
+     }
+
+     5. Package
+     package com.youtube
+     class Employee
+     {
+        code
+     }
+
+     6. Constant
+
+     class Employee
+     {
+        static final int MIN_AGE = 18;
+           code
+     }
+
+
+
+
+
+
+
+
+
+
+ */
